5af514c19768823e5d3d36ae28207b527203035e
DavidTang894 <davidtang894@gmail.com>
2025-05-02 22:20:50 +0800

    Small Add: Insert an option data to receive 2 info when select different option

--- src/pages/NonRegister/Products.jsx
+++ src/pages/NonRegister/Products.jsx
@@ -7,6 +7,9 @@ import { fetchProducts, fetchProductItem, fetch_ProductVariation } from "../../f
 const ProductPage  = () => {
     const { products, productItem, itemVariation } = useSelector((state) => state.products);
     const [product, setProduct] = useState({});
+    const [quantity, setQuantity] = useState(0);
+    const [selectedVariation, setSelectedVariation] = useState(0);
+    const [selectCharge, setSelectCharge] = useState(0);
     const { id } = useParams();
 
     const navigate = useNavigate();
@@ -35,25 +38,57 @@ const ProductPage  = () => {
             <section>
                 <h1>Product #{id}</h1>
                 <p>Here's the preview of the product</p>
-                <hr/>
-                <h2>{product.name}</h2>
             </section>
 
-            <section className="flex flex-col gap-2 bg-slate-200">
-                {productItem
-                    .filter(item => item.product_id == id)
-                    .map((item) => 
-                    <div>
-                        <p>Color: {itemVariation.find(data => data.variation_option_id == item.variation_option_id)?.value}</p>
-                        <p>Size: {itemVariation.find(data => data.variation_option_id == item.variation_option_2_id)?.value}</p>
-                        <p>quantity: {item.quantity}</p>
-                        {parseFloat(item.extra_charge) > 0 && <p>Extra Charge: item.extra_charge</p>}
-                    </div>
-                )}  
+            {product != null && (
+                <section className="bg-yellow-100">
+                    <h2>Name: {product.name}</h2>
+                    <p>Category ID: {product.category_id}</p>
+                    <p>Description: {product.description}</p>
+                    <p>Base Price: MYR{product.base_price}</p>
+                    <p>SKU: {product.sku}</p>
+                    {(product.created_date != undefined) && <p>Created Date: {product.created_date.split("T")[0].split("-").join("/")}</p>}
+                </section>
+            )}
+
+
+
+            <section className="flex flex-col gap-2 bg-slate-200 my-4 p-4">
+                <select 
+                    className="bg-slate-600 text-white py-4 px-4 rounded-lg" 
+                    onChange={(e) => {
+                        setSelectedVariation(e.target.selectedOptions[0].dataset.id); 
+                        setSelectCharge(e.target.selectedOptions[0].dataset.extra);
+                    }}>
+
+                    {productItem
+                        .filter(item => item.product_id == id)
+                        .map((item) => {
+                            const color = itemVariation.find(data => data.variation_option_id == item.variation_option_id)?.value;
+                            const size = itemVariation.find(data => data.variation_option_id == item.variation_option_2_id)?.value;
+                            const extra = parseFloat(item.extra_charge) > 0 ? ` | Extra Charge: ${item.extra_charge}` : "";
+                            return (
+                                <option key={item.id} value={item.id} data-id={item.id} data-extra={item.extra_charge}>
+                                    COLOR: {color} | SIZE: {size} | Stock: {item.quantity} {extra}
+                                </option>
+                            )
+                        }
+                    )} 
+                </select>
+            </section>
+
+            <section className="flex flex-col gap-4 my-4">
+                <input 
+                    type="number" 
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)} 
+                    className="min-w-fit w-60 border border-black py-2 px-4"
+                />
+                <button className="min-w-fit w-60 border border-black py-2 px-4 rounded-lg">Add Cart</button>
             </section>
 
             <section>
-                <button>Add Cart</button>
+                
                 <button>Add Wishlist</button>
             </section>
         </>
